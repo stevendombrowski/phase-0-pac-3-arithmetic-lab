ce00dfae5cb4cbda5426bb525c764b6c14730e25
Steven <dombrowskis153@yahoo.com>
2021-08-20 06:06:50 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+    add(1 + 80)
+}
+function subtract() {
+    subtract(60 - 40)
+}
+function multiply() {
+    multiply(2 * 3.4)
+}
+function divide(){
+    divide(5.0 / 2.5)
+}
+
+function increment(n){
+
+    n++;
+    return n;
+  }
+  function decrement(n){
+
+    n--;
+    return n;
+  }
+  function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b
+}
+function divide(a, b) {
+    return a / b
+}
+function multiply(a, b) {
+    return a * b
+
+}
+function makeInt(n){
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n){
+    return parseFloat(n);
+  }
